dcbb9c1cc16d9fe4ed3d3c2b011b54775a72c425
Francesco Palma <117196189+FrancescoPalmaDev@users.noreply.github.com>
2026-07-09 18:00:13 +0200

    handleNotes logic

--- main.go
+++ main.go
@@ -1,17 +1,35 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
+
+type Note struct {
+	Id   int
+	Text string
+}
+
+var notes []Note
 
 func main() {
+
+	notes = append(notes, Note{Id: 1, Text: "Test note"})
 	http.HandleFunc("/notes", handleNotes)
 	http.HandleFunc("/notes/", handleNotesById)
 	http.ListenAndServe(":8080", nil)
+
 }
 
 func handleNotes(writer http.ResponseWriter, request *http.Request) {
-
+	switch request.Method {
+	case "GET":
+		for _, value := range notes {
+			fmt.Fprintf(writer, "%d - %s", value.Id, value.Text)
+		}
+	}
 }
 
-func handleNotesById(write http.ResponseWriter, request *http.Request) {
+func handleNotesById(writer http.ResponseWriter, request *http.Request) {
 
 }
